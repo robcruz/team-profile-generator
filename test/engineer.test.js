9daed6fb0e2e4b8598cdad99e7ba5c3a9f2c7f5b
Rob <alekseicruz@gmail.om>
2020-03-14 02:59:05 -0700

    Engineer test... done!

--- test/engineer.test.js
+++ test/engineer.test.js
@@ -1,48 +1,6 @@
 const Engineer = require("../lib/engineer");
 
 describe("Engineer", () => {
-    describe("getName", () => {
-        it("should return name", () => {
-            // Arrange
-            const name = 'Dev Queen';
-            const id = 1;
-            const email = "alekseicruz@gmail.com";
-            const github = 'robcruz'
-            const engineer = new Engineer(name, id, email, name);
-
-            // Assert
-            expect(engineer.getName()).toEqual(name)
-        })
-    });
-
-    describe("getId", () => {
-        it("should return ID", () => {
-            // Arrange
-            const name = 'Dev Queen';
-            const id = 2;
-            const email = "alekseicruz@gmail.com"
-            const github = 'robcruz'
-            const engineer = new Engineer(name, id, email, id);
-
-            // Assert
-            expect(engineer.getId()).toEqual(id)
-        })
-    });
-
-    describe("getEmail", () => {
-        it("should return Email", () => {
-            // Arrange
-            const name = 'Dev Queen';
-            const id = 3;
-            const email = "alekseicruz@gmail.com"
-            const github = 'robcruz'
-            const engineer = new Engineer(name, id, email, email);
-
-            // Assert
-            expect(engineer.getEmail()).toEqual(email)
-        })
-    });
-
     describe("getRole", () => {
         it("should return Role", () => {
             // Arrange
