55f6dc576268f4f52159372a8001bce376e8f318
Rob <alekseicruz@gmail.om>
2020-03-15 02:11:30 -0700

    implemented employee role

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const inquirer = require("inquirer");
 const fs = require('fs');
 
-require("./lib/employee");
+require("./lib/employee")
 const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
 
 start();
 
@@ -25,8 +27,6 @@ async function start() {
         }
     ]);
 
-    console.log()
-
     let teamManager = new Manager();
     teamManager.name = manager.name;
     teamManager.email = manager.email;
@@ -35,45 +35,86 @@ async function start() {
 
     employees.push(teamManager);
 
-    console.log(employees)
+    while(true) {
+        let employee = await inquirer.prompt({
+            type: "checkbox",
+            message: "Choose employee role you want to enter next:",
+            name: "role",
+            choices: [
+                "Engineer",
+                "Intern",
+                "Done"
+            ]
+        });
 
+        let role = employee.role.shift();
+        if (role === 'Engineer') {
+            let engineer = await inquirer.prompt([
+            {
+                message: "Enter engineer's name:",
+                name: "name"
+            },
+            {
+                message: "Enter engineer's email:",
+                name: "email"
+            },
+            {
+                message: "Enter engineer's Github account:",
+                name: "github"
+            }
+        ]);
 
+            let teamEngineer = new Engineer();
+            teamEngineer.name = engineer.name;
+            teamEngineer.email = engineer.email;
+            teamEngineer.github = engineer.github;
+            teamEngineer.id = engineer.length + 1;
+        } else if (role === 'Intern') {
+            console.log(role)
+        } else {
+            break;
+        }
+        console.log(employee)
+    }
 
-    // let { manager, stack, contact } = await inquirer.prompt([
-    //     {
-    //         type: "input",
-    //         message: "What is the manager's name?",
-    //         name: "manager"
-    //     },
-    //     {
-    //         type: "checkbox",
-    //         message: "What languages do you know?",
-    //         name: "stack",
-    //         choices: [
-    //             "HTML",
-    //             "CSS",
-    //             "JavaScript",
-    //             "MySQL"
-    //         ]
-    //     },
-    //     {
-    //         type: "list",
-    //         message: "What is your preferred method of communication?",
-    //         name: "contact",
-    //         choices: [
-    //             "email",
-    //             "phone",
-    //             "telekinesis"
-    //         ]
-    //     }
-    // ]);
-
-
-    // console.log(`Manager: ${manager}`)
-    // console.log(`Stack: ${stack}`)
-    // console.log(`Contact: ${contact}`)
 
 }
+
+
+// let { manager, stack, contact } = await inquirer.prompt([
+//     {
+//         type: "input",
+//         message: "What is the manager's name?",
+//         name: "manager"
+//     },
+//     {
+//         type: "checkbox",
+//         message: "What languages do you know?",
+//         name: "stack",
+//         choices: [
+//             "HTML",
+//             "CSS",
+//             "JavaScript",
+//             "MySQL"
+//         ]
+//     },
+//     {
+//         type: "list",
+//         message: "What is your preferred method of communication?",
+//         name: "contact",
+//         choices: [
+//             "email",
+//             "phone",
+//             "telekinesis"
+//         ]
+//     }
+// ]);
+
+
+// console.log(`Manager: ${manager}`)
+// console.log(`Stack: ${stack}`)
+// console.log(`Contact: ${contact}`)
+
 
 // let { manager, stack, contact } = await inquirer.prompt([
 //     {
